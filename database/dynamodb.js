5c885ee267c1a8d93e7026910a44b524f282a153
Daniele Salvagni <daniele.salvagni@gmail.com>
2022-09-27 14:01:00 +0200

    Implement settings for offline DynamoDB

---
+++ database/dynamodb.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+const HOST = process.env.LOCAL_DDB_HOST || 'localhost';
+const PORT = process.env.LOCAL_DDB_PORT || 8000;
+const ENDPOINT = `http://${HOST}:${PORT}`;
+
+const OFFLINE_OPTIONS = {
+    region: 'localhost',
+    endpoint: ENDPOINT,
+    accessKeyId: 'MOCK_ACCESS_KEY_ID',
+    secretAccessKey: 'MOCK_SECRET_ACCESS_KEY',
+};
+
+const IS_OFFLINE = process.env.IS_OFFLINE || process.env.IS_LOCAL;
+
+exports.documentClient = IS_OFFLINE
+  ? new AWS.DynamoDB.DocumentClient(OFFLINE_OPTIONS)
+  : new AWS.DynamoDB.DocumentClient();
