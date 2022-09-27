c602628b1a4065c3585bc1fffaaf15891b676e74
Daniele Salvagni <daniele.salvagni@gmail.com>
2022-09-27 15:19:13 +0200

    Allow for additional headers to be added

--- http-api/response.js
+++ http-api/response.js
@@ -1,16 +1,17 @@
 'use strict'
 
-module.exports.create = (status, data = {}) => {
+module.exports.create = (status, body = {}, additionalHeaders) => {
 
   return {
     statusCode: status,
 
     headers: {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*",
+      ...additionalHeaders
     },
 
-    body: JSON.stringify(data)
+    body: JSON.stringify(body)
   };
 
 };
